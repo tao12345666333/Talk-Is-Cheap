8cd1bd261a2ccfc72c94fd3548d27d5c466b941b
TaoBeier <zhangjintao9020@gmail.com>
2016-01-13 22:59:04 +0800

    web security

---
+++ python/tornado/simple/web.Security.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+import logging
+
+import tornado.escape
+import tornado.gen
+import tornado.httpclient
+import tornado.ioloop
+import tornado.locale
+import tornado.web
+
+
+class WebHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        if not self.get_cookie('mycookie'):
+            self.set_cookie('mycookie', 'mycookievalue')
+            self.write('cookie not set yet')
+        else:
+            self.write('cookie set')
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        if not self.get_secure_cookie('mycookie'):
+            self.set_secure_cookie('mycookie', 'mycookievalue')
+            self.write('cookie_secret not set yet')
+        else:
+            self.write('cookie_secret set')
+
+
+if __name__ == '__main__':
+    settings = {
+        'debug': True,
+        'compiled_template_cache': False,
+        'cookie_secret': 'test_cookie'
+    }
+
+    app = tornado.web.Application([
+        (r'/', WebHandler),
+        (r'/main', MainHandler)
+    ], **settings)
+
+    app.listen(9999)
+    logging.warning('start ..')
+    tornado.ioloop.IOLoop.current().start()
