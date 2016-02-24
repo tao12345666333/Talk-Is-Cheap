3b3ccbb29e0ae2abac1543fad9f8a5689635e1aa
TaoBeier <zhangjintao9020@gmail.com>
2016-02-25 00:20:57 +0800

    mako template lib

--- python/tornado/simple/web.Request.py
+++ python/tornado/simple/web.Request.py
@@ -11,6 +11,9 @@ import tornado.httpclient
 import tornado.escape
 import tornado.locale
 
+import mako.lookup
+import mako.template
+
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -129,6 +132,29 @@ class PageHandler(tornado.web.RequestHandler):
         self.render('template.html', title='test', items=map(str, range(10)))
 
 
+class TemplateHandler(tornado.web.RequestHandler):
+
+    def initialize(self):
+        template_path = '.'
+        self.lookup = mako.lookup.TemplateLookup(directories=[template_path],
+                                                 input_encoding='utf-8',
+                                                 output_encoding='utf-8')
+
+    def render_string(self, template_path, **kwargs):
+        template = self.lookup.get_template(template_path)
+        namespace = self.get_template_namespace()
+        namespace.update(kwargs)
+        return template.render(**namespace)
+
+    def render(self, template_path, **kwargs):
+        self.finish(self.render_string(template_path, **kwargs))
+
+    def get(self):
+        logging.info('template handler')
+
+        self.render('mako_template.html', content='Hello Mako!')
+
+
 if __name__ == '__main__':
     settings = {
         'debug': True,
@@ -143,7 +169,8 @@ if __name__ == '__main__':
         (r'/redirect', RedirectHandler, {'url': '/file'}),
         (r'/webredirect', tornado.web.RedirectHandler, {'url': '/file'}),
         (r'/async', AsyncHandler),
-        (r'/page', PageHandler)
+        (r'/page', PageHandler),
+        (r'/template', TemplateHandler)
     ], **settings)
 
     app.listen(9999)
