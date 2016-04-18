44d20e1321414878ac228b1fd5f26676afa8693f
TaoBeier <zhangjintao9020@gmail.com>
2016-04-19 00:43:35 +0800

    some function

--- vue/vue-demo/src/components/Preview.vue
+++ vue/vue-demo/src/components/Preview.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="preview">
     <h1 v-if="true">{{ msg }}</h1>
-    <template v-if="false">
-      <h1>if {{ nmsg }}</h1>
+    <template v-if="true">
+      <div v-for="(i, m) in nmsg">
+        the {{ i }} is {{ m }}
+      </div>
     </template>
     <template v-else>
       <h1>else {{msg}}</h1>
     </template>
+
+    <div>
+      <span v-for="n in 10">{{ n }}</span>
+    </div>
+  </div>
+
+  <div class="preview">
+    <div>
+      <button v-on:click="on_btn">Click</button>
+    </div>
+  </div>
+
+  <div class="preview">
+    <div>
+      <div>Input: {{ t }}</div>
+      <input type="text" v-model="t" placeholder="input here">
+    </div>
   </div>
 </template>
 
@@ -15,7 +34,7 @@ export default {
   data() {
     return {
       msg: 'Hello World!',
-      nmsg: 'vim',
+      nmsg: ['a', 'b', 'c'],
     };
   },
 };
