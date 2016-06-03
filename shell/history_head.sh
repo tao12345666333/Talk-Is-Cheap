71a7713c7e77f2bd6808727dbd181eada76469fb
TaoBeier <zhangjintao9020@gmail.com>
2016-06-03 23:56:20 +0800

    add awk

---
+++ shell/history_head.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# get history top 10
+# by: Toy
+
+history | awk '{CMD[$2]++; count++;} END { for (a in CMD) print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n 10
