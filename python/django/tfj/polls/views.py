fe544641c78aac1fe230f31493f49de8818e4006
TaoBeier <zhangjintao9020@gmail.com>
2016-06-12 23:55:22 +0800

    django part 4

--- python/django/tfj/polls/views.py
+++ python/django/tfj/polls/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.template import loader
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
@@ -31,8 +51,19 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse('the results of question %s.' % question_id)
+    # return HttpResponse('the results of question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse('you vote on question %s.' % question_id)
+    # return HttpResponse('you vote on question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': 'you did not select a choice',})
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
