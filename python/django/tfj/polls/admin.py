7a69d086199aee2241991ec071e7800834762b56
TaoBeier <zhangjintao9020@gmail.com>
2016-06-15 00:10:00 +0800

    django part 7

--- python/django/tfj/polls/admin.py
+++ python/django/tfj/polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Content',          {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
