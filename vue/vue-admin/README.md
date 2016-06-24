c0a6ccd01f9343493da4e1247426a06fb61f1dcb
TaoBeier <zhangjintao9020@gmail.com>
2016-06-24 17:00:37 +0800

    one cooking demo

---
+++ vue/vue-admin/README.md
@@ -0,0 +1,16 @@
+# vue-admin
+> A vue admin project.
+
+## Development
+
+```shell
+make dev
+```
+
+## Production
+```
+make dist
+```
+
+## License
+MIT
---
+++ vue/vue-admin/src/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>vue-admin</h1>
+  <p>A vue admin project.</p>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  };
+</script>
