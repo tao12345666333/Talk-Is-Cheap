231d59363f0065db8de033493903e465b8bfe244
TaoBeier <zhangjintao9020@gmail.com>
2016-07-04 15:43:10 +0800

    stash

---
+++ vue/vue-demo/src/views/topicOne.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ topic.title }}/h1>
+
+    <div>
+      <div>{{ topic.content }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      topic: Object,
+    };
+  },
+}; 
+
+</script>
+
+<style>
+</style>
