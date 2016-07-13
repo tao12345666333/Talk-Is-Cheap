7a1909e271a1119fd97edee05d1cbc48b8cc1e04
TaoBeier <zhangjintao9020@gmail.com>
2016-07-13 19:11:11 +0800

    change store

--- vue/vue-demo/src/vuex/store.js
+++ vue/vue-demo/src/vuex/store.js
@@ -4,9 +4,43 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
+  notes: [],
+  activeNote: {},
+  show: '',
 };
 
 const mutations = {
+  initStore(state, data) {
+    state.notes = data.notes;
+    state.show = data.show;
+    state.activeNote = data.activeNote;
+  },
+
+  newNote(state) {
+    const nnote = {
+      id: +new Date(),
+      title: '',
+      content: '',
+    };
+
+    state.notes.push(nnote);
+    state.activeNote = nnote;
+  },
+
+  editNote(state, note) {
+    state.activeNote = note;
+
+    for(let i =0; i < state.notes.length; i++) {
+      if(state.notes[i].id === note.id) {
+        state.notes[i] = note;
+        break;
+      }
+    }
+  },
+
+  setActiveNote(state, note) {
+    state.activeNote = notes;
+  },
 };
 
 export default new Vuex.Store({
