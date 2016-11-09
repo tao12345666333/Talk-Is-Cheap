93ccc755f214f2f9162250a1e1917ab8af25abaf
TaoBeier <zhangjintao9020@gmail.com>
2016-11-10 01:08:46 +0800

    stash

--- go/more-type.go
+++ go/more-type.go
@@ -22,4 +22,18 @@ func main() {
 
 	fmt.Println(s.Y)
 	fmt.Println(sp)
+
+	a := make([]int, 5)
+
+	fmt.Println(cap(a))
+	fmt.Println(len(a))
+	fmt.Println(a)
+
+	printSlice("a", a)
+
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
