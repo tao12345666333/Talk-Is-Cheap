b5d0c99661233d1b73ee19c7e50db4167fc34d5d
TaoBeier <zhangjintao9020@gmail.com>
2016-12-18 23:37:11 +0800

    change

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
