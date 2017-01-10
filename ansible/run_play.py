9238c47753ca9851f5d9fc26a17af4951a85d0da
TaoBeier <zhangjintao9020@gmail.com>
2017-01-11 01:01:36 +0800

    update ansible

--- ansible/run_play.py
+++ ansible/run_play.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
 import json
+
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
 from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
 from ansible.executor.playbook_executor import PlaybookExecutor
-loader = DataLoader()                     # 用来加载解析yaml文件或JSON内容,并且支持vault的解密
+
+
+loader = DataLoader()  # 用来加载解析yaml文件或JSON内容,并且支持vault的解密
+
 # 管理变量的类,包括主机,组,扩展等变量,之前版本是在 inventory 中的
 variable_manager = VariableManager()
 inventory = Inventory(loader=loader, variable_manager=variable_manager)
+
 variable_manager.set_inventory(inventory)  # 根据 inventory 加载对应变量
 
 
@@ -28,16 +33,19 @@ class Options(object):
 
     def __getattr__(self, name):
         return None
-options = Options()
 
 
 def run_playbook():
+    options = Options()
     playbooks = ['task.yaml']  # 这里是一个列表, 因此可以运行多个playbook
     variable_manager.extra_vars = {
-        "ansible_ssh_user": "root", "ansible_ssh_pass": "xxx"}  # 增加外部变量
+        "ansible_ssh_user": "tao", "ansible_ssh_pass": "xxx"}  # 增加外部变量
+
     pb = PlaybookExecutor(playbooks=playbooks, inventory=inventory,
                           variable_manager=variable_manager, loader=loader, options=options, passwords=None)
     result = pb.run()
     print result
+
+
 if __name__ == '__main__':
     run_playbook()
